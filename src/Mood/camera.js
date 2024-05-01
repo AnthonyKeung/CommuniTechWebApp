23f354f42c58a06e2310e7e154310910a76eb519
AnthonyKeung <anthonykeung2468@gmail.com>
2024-05-02 00:27:00 +0100

    Stil going

---
+++ src/Mood/camera.js
@@ -0,0 +1,11 @@
+import "./camera.css"
+
+function camera() {
+    return(
+        <div className="videoOutput">
+            <h1> Camera Feed</h1>
+        </div>
+    )
+};
+
+export default camera
--- src/Mood/moodAnalysis.js
+++ src/Mood/moodAnalysis.js
@@ -1,9 +1,20 @@
 import "./moodAnalysis.css";
+import faceEmoji from "./faceEmoji";
+import camera from "./camera"
 
 function moodAnalysis(){
   return (
     <div>
-        <h1 className="title" style={{ color: '#C7A003' }}>Mood Analysis</h1>
+        <div>
+            <h1 className="title" style={{ color: '#C7A003' }}>Mood Analysis</h1>
+        </div>
+        <div className="videofeed-container">
+            {camera()}
+        </div>
+
+        <div classname="faces">
+            {faceEmoji()}
+        </div>
     </div>
   );
 }
