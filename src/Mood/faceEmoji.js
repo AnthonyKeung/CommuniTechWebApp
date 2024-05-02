aac1ce95d63cd3e533fdf2ce3bc5029b72ea215b
AnthonyKeung <anthonykeung2468@gmail.com>
2024-05-02 22:02:40 +0100

    Smiley faces space out nicely absolute pain

--- src/Mood/faceEmoji.js
+++ src/Mood/faceEmoji.js
@@ -1,13 +1,19 @@
 import "./faceEmoji.css"
 
 function faceEmoji() {
-  return (
-    <div className="emojiContainer">
-      <img src={require("./static/sadFace.png")} alt="sad" />
-      <img src={require("./static/neutralFace.png")} alt="neutral" />
-      <img src={require("./static/smileyFace.png")}alt="Happy" />
-    </div>
-  );
+    return (
+        <div className="emojiContainer">
+            <div>
+                <img src={require("./static/sadFace.png")} alt="sad"/>
+                {/*<img className="tapered_square" src={require("./static/tapered_square.png")} alt="Square"/>*/}
+            </div>
+            <di>
+                <img src={require("./static/neutralFace.png")} alt="neutral"/>
+            </di>
+            <div>
+                <img src={require("./static/smileyFace.png")} alt="Happy"/>
+            </div>
+        </div>
+    );
 }
-
 export default faceEmoji;
--- src/Mood/moodAnalysis.js
+++ src/Mood/moodAnalysis.js
@@ -2,21 +2,19 @@ import "./moodAnalysis.css";
 import faceEmoji from "./faceEmoji";
 import camera from "./camera"
 
-function moodAnalysis(){
-  return (
-    <div>
-        <div>
-            <h1 className="title" style={{ color: '#C7A003' }}>Mood Analysis</h1>
-        </div>
-        <div className="videofeed-container">
-            {camera()}
-        </div>
+function moodAnalysis() {
+    return (
+        <div className="mood-container">
+            <div>
+                <h1 className="title" style={{color: '#C7A003'}}>Mood Analysis</h1>
+                {faceEmoji()}
+            </div>
+            <div className="videofeed-container">
+                {camera()}
+            </div>
 
-        <div classname="faces">
-            {faceEmoji()}
         </div>
-    </div>
-  );
+    );
 }
 
 export default moodAnalysis;
