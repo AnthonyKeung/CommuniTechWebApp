e0f2b66942526a2a016d499d66f05dd5d56e607d
AnthonyKeung <anthonykeung2468@gmail.com>
2024-05-02 23:46:47 +0100

    Graphs looking nice aswell

--- src/Plants/PlantHealth.js
+++ src/Plants/PlantHealth.js
@@ -4,11 +4,17 @@ import "./PlantHealth.css";
 const PlantHealth = () => {
     return (
         <div>
-            <h1 className="plantTitle" style={{ color: '#C7A003' }}>Plant Health</h1>
-            <div className="container">
-                <PlantGraph plantInfo="Temperature"/>
-                <PlantGraph plantInfo="Humidity"/>
-                <PlantGraph plantInfo="light"/>
+            <h1 className="plantTitle" style={{color: '#C7A003'}}>Plant Health</h1>
+            <div className="graphs-container">
+                <div className = 'graph-container'>
+                    <PlantGraph plantInfo="Temperature"/>
+                </div>
+                <div className = 'graph-container'>
+                    <PlantGraph plantInfo="Humidity"/>
+                </div>
+                <div className = 'graph-container'>
+                    <PlantGraph plantInfo="light"/>
+                </div>
             </div>
         </div>
     );
