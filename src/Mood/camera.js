1d8083b526222a02c4fb701da1f147b3cb546eb6
AnthonyKeung <anthonykeung2468@gmail.com>
2024-05-02 20:36:05 +0100

    Video feed working

--- src/Mood/camera.js
+++ src/Mood/camera.js
@@ -1,11 +1,17 @@
 import "./camera.css"
 
 function camera() {
-    return(
-        <div className="videoOutput">
-            <h1> Camera Feed</h1>
+    return (
+        <div className="container">
+            <div className="row">
+                <div class="col-lg-8  offset-lg-2">
+                    <h1 className="title" style={{ color: '#C7A003' }}>Live Streaming</h1>
+                    <img src="http://localhost:5000/video_feed" width="100%" alt="Video not showing"/>
+                </div>
+            </div>
         </div>
-    )
+
+)
 };
 
 export default camera
--- src/Mood/faceEmoji.js
+++ src/Mood/faceEmoji.js
@@ -3,7 +3,7 @@ import "./faceEmoji.css"
 function faceEmoji() {
   return (
     <div className="emojiContainer">
-      <img src={require("C:\\Bath\\Year 3\\GDBP\\communitech_wall_app\\src\\Mood\\static\\sadFace.png")} alt="sad" />
+      <img src={require("./static/sadFace.png")} alt="sad" />
       <img src={require("./static/neutralFace.png")} alt="neutral" />
       <img src={require("./static/smileyFace.png")}alt="Happy" />
     </div>
